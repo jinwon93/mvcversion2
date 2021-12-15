006d1d3b1379461dd5df373ef6beb8984cac0650
jin <kjw8208@nate.com>
2021-12-15 11:16:31 +0900

    thy

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -24,10 +28,26 @@ public class BasicController {
         return "basic/text-unescapted";
     }
 
-//    @GetMapping("/variable")
-//    public String variable(Model model){
-//
-//    }
+    @GetMapping("/variable")
+    public String variable(Model model){
+
+        User userA = new User("userA" , 10);
+        User userB = new User("userB" , 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String , User> map = new HashMap<>();
+        map.put("userA" , userA);
+        map.put("userB" , userB);
+
+        model.addAttribute("user" , userA);
+        model.addAttribute("users" , list);
+        model.addAttribute("userMap" , map);
+
+        return "basic/variable";
+    }
 
     @GetMapping("/data")
     public  String date(Model model){
@@ -42,6 +62,21 @@ public class BasicController {
 
         return "basic/link";
 
+    }
+
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA" , 10));
+        list.add(new User("userB" , 20));
+        list.add(new User("userC" , 30));
+
+        model.addAttribute("users" , list);
     }
 
 
